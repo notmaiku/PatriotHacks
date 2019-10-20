cea9c05c5d0be1271167c876b8094f4224a69e6c
Tommy Lee <tlee32@gmu.edu>
2019-10-20 09:30:24 -0400

    Added currency formatting

--- src/ActorMetadata.jsx
+++ src/ActorMetadata.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { convertToCurrency } from './utils';
 
 const ActorMetadata = ({ name, budget, revenue }) => (
   <div>
     <div>{`Name: ${name}`}</div>
-    <div>{`Average Budget: ${budget}`}</div>
-    <div>{`Average Revenue: ${revenue}`}</div>
+    <div>{`Average Budget: ${convertToCurrency(budget)}`}</div>
+    <div>{`Average Revenue: ${convertToCurrency(revenue)}`}</div>
   </div>
 );
 
