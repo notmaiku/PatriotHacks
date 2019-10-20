3605c87119364d677fb53a367c469a83bf219ac8
notmaiku <notmaiku@protonmail.com>
2019-10-19 23:35:07 -0400

    added some basic styling for left panel

--- src/ActorPanel/ActorPanelList.jsx
+++ src/ActorPanel/ActorPanelList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const actors = ['Adam Sandler', 'Brad Pitt', 'Tom Cruise', 'Tom Hanks'];
-const ActorPanelList = () => actors.map((actor) => <div key={Math.random()}>{actor}</div>);
+const ActorPanelList = () => actors.map((actor) => <li key={Math.random()}><p>{actor}</p></li>);
 
 export default ActorPanelList;
